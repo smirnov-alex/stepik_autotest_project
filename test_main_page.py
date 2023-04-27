4591b01ba1df944d4d959efc5214c8046581054d
Alex <smirnov1991alexx@gmail.com>
2023-04-27 17:04:39 +0500

    set test with class MainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -9,6 +8,8 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
