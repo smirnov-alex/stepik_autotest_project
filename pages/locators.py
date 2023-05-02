aae1437526d0915b4038aaf89b8147245ead8856
Alex <smirnov1991alexx@gmail.com>
2023-05-02 14:55:21 +0500

    add checking going to login page

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+    LOGIN_USERNAME = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_SUBMIT = (By.CSS_SELECTOR, '#id_login-password')
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTRATION_PASSWORD_CONFIRM = (By.CSS_SELECTOR, '#id_registration-password2')
